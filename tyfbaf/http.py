a43c3886e348fb80b1e18e9b3a0937fb856540d7
yveso <yveso@users.noreply.github.com>
2022-02-23 19:44:03 +0100

    get function in http module

--- tyfbaf/http.py
+++ tyfbaf/http.py
@@ -1,4 +1,3 @@
-from tkinter.messagebox import NO
 from typing import Any
 import httpx
 
@@ -14,7 +13,18 @@ def _setup(server_name: str, port: int = 6405) -> None:
     PORT = port
 
 
-def post(endpoint: str, *, body=None, token=None) -> dict[str, Any]:
+def get(endpoint: str, *, token: str = None) -> dict[str, Any]:
+    response = httpx.get(
+        f"http://{SERVER_NAME}:{PORT}/biprws{endpoint}",
+        proxies=PROXIES,
+        headers=(
+            {"X-SAP-LogonToken": token, **BASE_HEADERS} if token else BASE_HEADERS
+        ),
+    )
+    return response.json() if response.text else {}
+
+
+def post(endpoint: str, *, body: dict = None, token: str = None) -> dict[str, Any]:
     response = httpx.post(
         f"http://{SERVER_NAME}:{PORT}/biprws{endpoint}",
         proxies=PROXIES,
