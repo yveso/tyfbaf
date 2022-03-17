5111b99d0eb0a8786b7d8bf3457a06ec4c4ba021
yveso <yveso@users.noreply.github.com>
2022-03-17 21:58:25 +0100

    Tests for http module

--- tests/test_http.py
+++ tests/test_http.py
@@ -1,11 +1,191 @@
+import json
+import httpx
 import respx
-from tyfbaf import constants
-from tyfbaf.http import get
+from tyfbaf import constants, http
 
 
-@respx.mock
-def test_get_uri():
-    constants.SERVER_NAME = "a"
-    foo_route = respx.get("http://a:6405/biprws/foo").mock()
-    get("/foo")
-    assert foo_route.called
+def test_get_expected_uri_gets_called(respx_mock):
+    constants.SERVER_NAME = "server_name"
+    route = respx.get("http://server_name:6405/biprws/an_endpoint")
+    _ = http.get("/an_endpoint")
+    assert route.called
+
+
+def test_get_base_headers_are_set(respx_mock):
+    constants.SERVER_NAME = "server_name"
+    route = respx.get(
+        headers={
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+    )
+    _ = http.get("/an_endpoint")
+    assert route.called
+
+
+def test_get_token_if_passed_is_used(respx_mock):
+    constants.SERVER_NAME = "server_name"
+    route = respx.get(
+        headers={
+            "X-SAP-LogonToken": "a_token",
+        },
+    )
+    _ = http.get("/an_endpoint", token="a_token")
+    assert route.called
+
+
+def test_get_token_if_saved_is_used(respx_mock):
+    constants.SERVER_NAME = "server_name"
+    constants.CURRENT_TOKEN = "saved_token"
+    route = respx.get(
+        headers={
+            "X-SAP-LogonToken": "saved_token",
+        },
+    )
+    _ = http.get("/an_endpoint")
+    assert route.called
+
+
+def test_get_returns_empty_dict_if_response_was_empty(respx_mock):
+    constants.SERVER_NAME = "server_name"
+    _ = respx.get().mock(return_value=httpx.Response(200, text=""))
+    response = http.get("/an_endpoint")
+    assert response == {}
+
+
+def test_get_returns_expected_object(respx_mock):
+    constants.SERVER_NAME = "server_name"
+    _ = respx.get().mock(return_value=httpx.Response(200, json={"key": "value"}))
+    response = http.get("/an_endpoint")
+    assert response == {"key": "value"}
+
+
+def test_post_expected_uri_gets_called(respx_mock):
+    constants.SERVER_NAME = "server_name"
+    route = respx.post("http://server_name:6405/biprws/an_endpoint")
+    _ = http.post("/an_endpoint")
+    assert route.called
+
+
+def test_post_base_headers_are_set(respx_mock):
+    constants.SERVER_NAME = "server_name"
+    route = respx.post(
+        headers={
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+    )
+    _ = http.post("/an_endpoint")
+    assert route.called
+
+
+def test_post_token_if_passed_is_used(respx_mock):
+    constants.SERVER_NAME = "server_name"
+    route = respx.post(
+        headers={
+            "X-SAP-LogonToken": "a_token",
+        },
+    )
+    _ = http.post("/an_endpoint", token="a_token")
+    assert route.called
+
+
+def test_post_token_if_saved_is_used(respx_mock):
+    constants.SERVER_NAME = "server_name"
+    constants.CURRENT_TOKEN = "saved_token"
+    route = respx.post(
+        headers={
+            "X-SAP-LogonToken": "saved_token",
+        },
+    )
+    _ = http.post("/an_endpoint")
+    assert route.called
+
+
+def test_post_body_if_passed_is_used(respx_mock):
+    constants.SERVER_NAME = "server_name"
+    route = respx.post(
+        json={
+            "key": "value",
+        },
+    )
+    _ = http.post("/an_endpoint", body={"key": "value"})
+    assert route.called
+
+
+def test_post_returns_empty_dict_if_response_was_empty(respx_mock):
+    constants.SERVER_NAME = "server_name"
+    _ = respx.post().mock(return_value=httpx.Response(200, text=""))
+    response = http.post("/an_endpoint")
+    assert response == {}
+
+
+def test_post_returns_expected_object(respx_mock):
+    constants.SERVER_NAME = "server_name"
+    _ = respx.post().mock(return_value=httpx.Response(200, json={"key": "value"}))
+    response = http.post("/an_endpoint")
+    assert response == {"key": "value"}
+
+
+def test_post_full_uriexpected_uri_gets_called(respx_mock):
+    route = respx.post("http://server_name:6405/biprws/an_endpoint")
+    _ = http.post_full_uri("http://server_name:6405/biprws/an_endpoint")
+    assert route.called
+
+
+def test_post_full_uri_base_headers_are_set(respx_mock):
+    route = respx.post(
+        headers={
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+    )
+    _ = http.post_full_uri("http://server_name:6405/biprws/an_endpoint")
+    assert route.called
+
+
+def test_post_full_uri_token_if_passed_is_used(respx_mock):
+    route = respx.post(
+        headers={
+            "X-SAP-LogonToken": "a_token",
+        },
+    )
+    _ = http.post_full_uri(
+        "http://server_name:6405/biprws/an_endpoint", token="a_token"
+    )
+    assert route.called
+
+
+def test_post_full_uri_token_if_saved_is_used(respx_mock):
+    constants.CURRENT_TOKEN = "saved_token"
+    route = respx.post(
+        headers={
+            "X-SAP-LogonToken": "saved_token",
+        },
+    )
+    _ = http.post_full_uri("http://server_name:6405/biprws/an_endpoint")
+    assert route.called
+
+
+def test_post_full_uri_body_if_passed_is_used(respx_mock):
+    route = respx.post(
+        json={
+            "key": "value",
+        },
+    )
+    _ = http.post_full_uri(
+        "http://server_name:6405/biprws/an_endpoint", body={"key": "value"}
+    )
+    assert route.called
+
+
+def test_post_full_uri_returns_empty_dict_if_response_was_empty(respx_mock):
+    _ = respx.post().mock(return_value=httpx.Response(200, text=""))
+    response = http.post_full_uri("http://server_name:6405/biprws/an_endpoint")
+    assert response == {}
+
+
+def test_post_full_uri_returns_expected_object(respx_mock):
+    _ = respx.post().mock(return_value=httpx.Response(200, json={"key": "value"}))
+    response = http.post_full_uri("http://server_name:6405/biprws/an_endpoint")
+    assert response == {"key": "value"}
