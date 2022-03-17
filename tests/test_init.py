6ec9d055c55072938079d1310a5bbd69eacde108
yveso <yveso@users.noreply.github.com>
2022-03-17 22:55:45 +0100

    Tests for token module

--- tests/test_init.py
+++ tests/test_init.py
@@ -15,3 +15,4 @@ def test_setup_default_port_is_used_when_none_is_given():
 def test_setup_port_is_saved_as_intended_when_given():
     tyfbaf.setup("my-awesome-server-name", port=1234)
     assert tyfbaf.constants.PORT == 1234
+    tyfbaf.constants.PORT = 6405
